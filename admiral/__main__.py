376aa0fd0de0a00bb82f7fbfa8978df960c40359
Ricardo Amaro <ricardo.amaro@gmail.com>
2020-05-18 18:05:29 +0100

    restructure, cleanups

--- admiral/__main__.py
+++ admiral/__main__.py
@@ -17,52 +17,44 @@ ROUTES_METRICS = web.RouteTableDef()
 REF_CONTROLLER = RefController('/tmp', embed_refs=True)
 REVEALER = Revealer(REF_CONTROLLER)
 
+
 @ROUTES_METRICS.get('/metrics')
 async def metrics_handler(request):
     return web.Response(text="Metrics go here")
 
+
 @ROUTES.post('/mutate/{resource}')
 async def mutate_resource_handler(request):
-    resource = request.match_info.get('resource', None)
-    # XXX is this needed at all?
-    if resource is None:
-        return web.Response(status=500, reason='Resource not set')
-
     try:
         req_json = await request.json()
+        # default to 500 unknown error
+        response = web.Response(status=500, reason='Unknown error')
 
-        # check for annotation 'kapicorp.com/admiral: kapitan-embed-refs'
-        annotated_refs = False
-        try:
-            annotations = req_json["request"]["object"]["metadata"]["annotations"]
-            if annotations["kapicorp.com/admiral"] == "kapitan-embed-refs":
-                annotated_refs: True
+        # check for 'kapicorp.com/admiral' annotations
+        annotations = req_json["request"]["object"]["metadata"]["annotations"]
+        annotation = annotations.get("kapicorp.com/admiral", None)
 
-        # not annotated, default allow
-        except KeyError:
-            # TODO log success, default allow
+        if annotation == "kapitan-reveal-refs":
+            try:
+                req_copy = deepcopy(req_json)
+                reveal_req_func = lambda: kapitan_reveal_json(req_copy)
+                req_revealed = await run_blocking(reveal_req_func)
+                patch = make_patch(req_json, req_revealed)
+                response = make_response(patch, allow=True, message="")
+
+            except Exception as e:
+                # TODO log exception error
+                response = make_response([], allow=False,
+                                         message="Kapitan Reveal Failed")
+        else:
+            # not annotated, default allow
+            # TODO log success
             response = make_response([], allow=True, message="")
-
-        req_copy = deepcopy(req_json)
-
-        try:
-            reveal_req_func = lambda: kapitan_reveal_json(req_copy)
-            req_revealed = await run_blocking(reveal_req_func)
-            patch = make_patch(req_json, req_revealed)
-            response = make_response(patch, allow=True, message="")
-
-            return web.json_response(response)
-
-        except Exception as e:
-            # TODO log exception error
-            response = make_response([], allow=False, message="Kapitan Reveal Failed")
-
-            return web.json_response(response)
 
     except json.decode.JSONDecoderError:
         return web.Response(status=500, reason='Request not JSON')
 
-    return web.Response(status=500, reason='Unknown error')
+    return response
 
 
 def make_patch(src_json, dst_json):
@@ -73,25 +65,28 @@ def make_patch(src_json, dst_json):
 def make_response(patch, allow=False, message=""):
     patch_json = json.dumps(patch)
     b64_patch = b64encode(patch_json.encode()).decode()
-    return {
-            "response": {
-                "allowed": allow,
-                "status": {"message": message},
-                "patchType": "JSONPatch",
-                "patch": b64_patch
+    response = {
+                "response": {
+                    "allowed": allow,
+                    "status": {"message": message},
+                    "patchType": "JSONPatch",
+                    "patch": b64_patch
+                    }
                 }
-            }
+    return web.json_response(response)
 
 
 async def run_blocking(func):
     loop = asyncio.get_running_loop()
     return await loop.run_in_executor(None, func)
 
+
 def kapitan_reveal_json(json_doc):
     "return revealed object, total revealed tags (TODO)"
     return REVEALER.reveal_obj(json_doc)
 
-async def start_site(app, app_runners, address="localhost", port=8080):
+
+async def start_site(app, app_runners, address="0.0.0.0", port=8080):
     runner = web.AppRunner(app)
     app_runners.append(runner)
     await runner.setup()
